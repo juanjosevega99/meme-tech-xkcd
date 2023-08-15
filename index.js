2c4482fef6dcf7dc5448f6c166329ce23224da1a
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2023-08-14 20:57:15 -0500

    chore: save comics json

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 import fs from 'fs-extra';
 import axios from 'axios';
 
+const { writeJSON } = fs;
+
 const INITIAL_ID_XKCD_COMIC = 2500;
 const MAX_ID_XKCD_COMIC = 2600;
 
-for (
-  let comicId = INITIAL_ID_XKCD_COMIC;
-  comicId < MAX_ID_XKCD_COMIC;
-  comicId++
-) {
+for (let id = INITIAL_ID_XKCD_COMIC; id < MAX_ID_XKCD_COMIC; id++) {
   const url = `https://xkcd.com/${id}/info.0.json`;
   const { data } = await axios.get(url);
-  const { num: id, news, transcript, ...restOfComic } = data;
+  const { num, news, transcript, ...restOfComic } = data;
+  const comicToStore = {
+    id,
+    ...restOfComic,
+  };
 
-  fs.writeJSON(`./comics/${id}.json`, restOfComic, { spaces: 2 });
+  await writeJSON(`./comics/${id}.json`, comicToStore);
 }
